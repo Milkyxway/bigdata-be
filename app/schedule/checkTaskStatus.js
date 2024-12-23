1d382c9a6d4b6eaacef1b757abdf4b00069ae0b2
xjs <xjsww@1214@163.com>
2024-12-23 14:47:21 +0800

    fix: fix code

--- app/schedule/checkTaskStatus.js
+++ app/schedule/checkTaskStatus.js
@@ -12,23 +12,18 @@ class logTime extends Subscription {
 	}
 
 	async subscribe() {
-		const cycleExeTasks = [534,533,536]
-		await this.app.mysql.update("report_list", {
-			reportState: 1,
-			lastTime: '2020-01-01 00:00:00'
-		 }, {
-			where: {
-				reportId: 911,
-			},
-		});
-		await this.app.mysql.update("report_list", {
-			reportState: 1,
-			lastTime: '2020-01-01 00:00:00'
-		 }, {
-			where: {
-				reportId: 910,
-			},
-		});
+		const faultTasks = await this.app.mysql.query(`select * from report_list where reportId in (1136, 1134, 924, 921,918) and reportState = 4`)
+		faultTasks.map(async i => {
+			await this.app.mysql.update("report_list", {
+				reportState: 1,
+				lastTime: '2020-01-01 00:00:00'
+			 }, {
+				where: {
+					reportId: i.reportId,
+				},
+			});
+		})
+		
 		
 
 	}
